d9a9f339ce4c38770e01515777f7ed74090a1e67
ShRafa <rafa.ps89@gmail.com>
2023-08-16 20:08:02 -0300

    Docker update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -49,17 +49,21 @@ TEMPLATES = [
 WSGI_APPLICATION = "ecommerce.wsgi.application"
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.environ.get('DATABASE_NAME'),
+    #     'USER': os.environ.get('DATABASE_USER'),
+    #     'PASSWORD': os.environ.get('DATABASE_PASSWORD:'),
+    #     'HOST': os.environ.get('DATABASE_HOST'),
+    #     'PORT': os.environ.get('DATABASE_PORT'),
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD:'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Ecommerce@2023',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
